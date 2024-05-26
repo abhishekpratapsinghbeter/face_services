b353342785ce3a54b53501f8e0b60ff95fffcfd9
Abhishek Pratap Singh <abhishekpratapsingh1234@gmail.com>
2024-05-26 08:51:43 +0530

    cors update

--- app.js
+++ app.js
@@ -8,16 +8,17 @@ const faceapi = require('face-api.js');
 const path = require('path'); 
 const MODEL_URL = path.join(__dirname, './src/models/models'); // Path to your models directory
 
-const loadModels = async () => {
-    await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_URL);
-    await faceapi.nets.faceLandmark68Net.loadFromDisk(MODEL_URL);
-    await faceapi.nets.faceRecognitionNet.loadFromDisk(MODEL_URL);
-};
 
 dotenv.config({ path: './config.env' });
 require("./src/models/connection/conn");
 
-app.use(cors());
+const corsOptions = {
+    origin: 'https://attendxpert.onrender.com', // Allow requests from this origin
+    methods: ['GET', 'POST'], // Allow only GET and POST requests
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+  };
+  app.use(cors(corsOptions));
+  
 app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.json({ limit: '50mb' }));
